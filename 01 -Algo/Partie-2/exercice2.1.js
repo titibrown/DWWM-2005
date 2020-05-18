e63ac1c95e089beab917b7e4808f50067a18b838
Brown <brown@MBP-de-Brown.home>
2020-05-18 11:46:00 +0200

    Saisi Cours

--- 01 -Algo/Partie-2/exercice2.1.js
+++ 01 -Algo/Partie-2/exercice2.1.js
@@ -3,24 +3,27 @@
 
 //L'utilisateur saisit une valeur en kilomètres comprise entre 0.01 et 1 000 000. Si la valeur est hors limite, 
 //l'utilisateur est invité à saisir une nouvelle valeur. Si la valeur est égale à "q", le programme se termine et se ferme.
-
 //Formule km vers mi :
 //Le programme affiche le résultat de la conversion sous forme de nombre réel double précision.
-
-
 //Exemple d’affichage en mode Console :
- 
-  
 //Programme de conversion Kilomètres -> Miles. Saisir une valeur en Kilomètres :
 //17
 //17 Kilomètres = 10.5655 Miles
-//Appuyez sur une touche pour quitter.
+//Appuyez sur une touche pour quit ter.
+
+
+Variable float 
+
+Kilometres, miles, result
+
+DEBUT
+
 
 var text = 'Saisir une valeur en Kilomètre :', number= 
 var Kilometres;
 var Miles; 
 var result = Kilometres * 1.60
-
+var calcul =
 
 Kilometres = ('Saisir une valeur en Kilomètre:');
 result = Kilometres * 1,60934
@@ -29,12 +32,6 @@ result = Kilometres * 1,60934
 console.log(result);
 
 
-
-
-
-
-
-;
  
 //let variable1 = "kilomètre";
 //let variable2 = "Miles";
