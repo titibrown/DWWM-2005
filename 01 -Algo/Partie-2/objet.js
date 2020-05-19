0a4afa159cc628bc60dd5017aacd3ab029508d91
Brown <brown@MBP-de-Brown.home>
2020-05-19 17:20:19 +0200

    Starting

---
+++ 01 -Algo/Partie-2/objet.js
@@ -0,0 +1,37 @@
+function hello () {
+    console.log ( " Hello world !")
+}
+
+
+
+hello();
+hello(); 
+
+function welcome(){
+
+    return"welcome";
+}
+
+
+var w = welcome ();
+console.log (w);
+console.log(welcome());
+
+function talk (message){
+
+    console.log(message);
+}
+
+talk ("Bonjour !");
+talk ("Bonjour !");
+
+
+const hi = function (){
+
+    console.log("hi !")
+}
+
+hi (); 
+
+hi (); 
+
