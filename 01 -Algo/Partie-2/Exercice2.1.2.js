803da3f713773a2aaaba2f6e7cfaa8d47e2df0b8
Brown <brown@MBP-de-Brown.home>
2020-05-19 12:35:40 +0200

    Début

---
+++ 01 -Algo/Partie-2/Exercice2.1.2.js
@@ -0,0 +1,67 @@
+//Exercice 2.1.3 :
+//L’utilisateur peut saisir plusieurs valeurs à convertir.
+//Il saisit une valeur à convertir avec son unité de mesure(km ou mi).
+//Si aucune unité de mesure n’est indiquée, le programme considère la valeur en kilomètres.
+//A l’appui de la touche « Entrée », la valeur saisie est enregistrée dans un tableau en mémoire.
+//L’utilisateur peut enregistrer autant de valeurs qu’il souhaite.
+//Si l’utilisateur saisit la commande « go », le programme convertit toutes les valeurs mémorisées 
+//et affiche le résultat de chaque conversion.
+//La commande « quit » permet de quitter le programme.Tant que cette commande n’est pas saisie, 
+// l’utilisateur peut continuer à faire des conversions.
+
+
+// Pseudo Code 
+
+
+Variable EST UN ENTIER 
+
+km , mi , result
+
+
+DEBUT 
+ECRIRE "Bonjour et bienvenue sur Brownieconvertisseur"
+ECRIRE "Saisir une ou plusieur Valeurs avec l'unité de mesure souhaité"
+AFFICHER " SAISI UTILISATEUR "
+
+SI Aucune mesure est indiquée 
+   
+    Alors 
+    " Considèrer automatiquement la  ou les valeur saisi en Kilomètre"
+    si l'utilisateur valide avec entrée 
+    Alors 
+    Enregistrer dans un tableau 
+Si utilisateur saisi <go> 
+alors convertir toutes les valeurs mémorisées et afficher les resultat de chaque conversion 
+
+Tant que 
+
+commande <quit> n'est pas saisie alors continuer les conversions. 
+
+FIN 
+
+
+//Javascript
+
+let input ;
+let result ;
+let tab = [];
+input = entree_utilisateur;
+result = input * 1,60;
+
+do {
+    
+    console.log ("Saisir une ou plusieur Valeurs avec l'unité de mesure souhaité ");
+    input = entree_utilisateur
+    if (){
+       result = input * 1,60;
+       
+    }
+
+ else {
+    console.log("");
+
+ } 
+
+
+
+}while ( ! ( ));
