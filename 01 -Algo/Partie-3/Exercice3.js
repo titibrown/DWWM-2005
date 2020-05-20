f4b4bc5d68c04b4133f2e62d4d72e1ecffa86279
Brown <brown@MBP-de-Brown.home>
2020-05-20 15:55:12 +0200

    test et re test

---
+++ 01 -Algo/Partie-3/Exercice3.js
@@ -0,0 +1,21 @@
+//Exercice 3.1 : Rechercher un nombre dans un tableau
+//Soit un tableau de nombres triés par ordre croissant.
+//Chercher si un nombre donné N figure parmi les éléments.
+//Si oui, indiquer la valeur de l'indice ctableauespondant. Sinon, indiquer « Non trouvé ».
+
+
+
+
+
+
+var number = ['2, 4, 6, 8, 10'];
+console.log(number.length);
+
+number.length =5;
+console.log(number); 
+
+number.length = 0;
+console.log(number); 
+
+number.length = 3;
+console.log(number);
--- JS/Exercices /employees.js
+++ JS/Exercices /employees.js
@@ -37,82 +37,98 @@
 /**
  * @class Employee
  */
-class Employee
-{
-    Employee { 
-        id: 1,
+class Employee {
+    Employee {
+    id: 1,
         lastname: 'Trump',
-        firstname:'Donald ',
-        role:'creator of Fake News',
-        salary:500000,
-        hireDate:'2017-01-20',
-        email:'dtrump@email.fr'
+            firstname: 'Donald ',
+                role: 'creator of Fake News',
+                    salary: 500000,
+                        hireDate: '2017-01-20',
+                            email: 'dtrump@email.fr'
+}
+{
+    Employee {
+        id: 2,
+            lastname: ' Obama',
+                firstname: 'Barack',
+                    role: 'creator of hope',
+                        salary: 1000000,,
+                            hireDate: '2009-01-20',
+                                email: 'bobama@email.fr'
     }
+
     {
         Employee{
-            id: 2,
-            lastname: ' Obama',
-            firstname:'Barack',
-            role:'creator of hope',
-            salary:1000000,,
-            hireDate:'2009-01-20',
-            email:'bobama@email.fr'
+            id: 3,
+                lastname: 'Bush',
+                    firstname: 'George',
+                        role: 'responsable of Guantanamo',
+                            salary: 100000,
+                                hireDate: '2001-01-20',
+                                    email: 'gbush@email.fr'
         }
 
         {
             Employee{
-                id: 3,
-                lastname: 'Bush',
-                firstname:'George',
-                role:'responsable of Guantanamo',
-                salary: 100000,
-                hireDate:'2001-01-20',
-                email:'gbush@email.fr'
+                id: 4,
+                    lastname: 'Clinton',
+                        firstname: 'Bill',
+                            role: 'assistant of Lewinsky',
+                                salary: 50000,
+                                    hireDate: '1993-01-20',
+                                        email: 'bcliton@email.fr'
             }
 
             {
                 Employee{
-                    id: 4,
-                    lastname: 'Clinton',
-                    firstname:'Bill',
-                    role:'assistant of Lewinsky',
-                    salary: 50000,
-                    hireDate:'1993-01-20',
-                    email:'bcliton@email.fr'
+                    id: 5,
+                        lastname: 'Reagan',
+                            firstname: 'Ronadl',
+                                role: 'Actor',
+                                    salary: 10000,
+                                        hireDate: '1981-01-20',
+                                            email: 'rreagan@email.fr'
                 }
 
-                {
-                    Employee{
-                        id: 5,
-                        lastname: 'Reagan',
-                        firstname:'Ronadl',
-                        role:'Actor',
-                        salary:10000,
-                        hireDate:'1981-01-20',
-                        email:'rreagan@email.fr'
+
+
+
+
+                /** DÉBUT ZONE NON EDITABLE (Ne pas modifier les lignes suivantes) */
+
+
+                /** @var Employee employee1 */
+                var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-12-28')); // création d'un nouvel employé
+
+                /** @var array employees */
+                const employees = [employee1]; // tableau contenant les employés
+
+                console.log(employee1); // doit afficher l'objet "employee1"
+                console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
+                console.log(employees); // export des employés dans la console
+
+
+                /** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
+
+
+
+                // Écrivez votre code à partir de la ligne suivante...
+
+                let Employee = class {
+                    constructor(id, lastname, firstname, role, salary, hireDate, email) {
+                        this.id = id
+                        this.lastname = lastname;
+                        this.firstname = firstname;
+                        this.role = role ;
+                        this.salary = salary;
+                        this.hireDate = hireDate;
+                        this.email = email;
+
+
                     }
-                
+
+                    get
 
 
-
-
-/** DÉBUT ZONE NON EDITABLE (Ne pas modifier les lignes suivantes) */
-
-
-/** @var Employee employee1 */
-var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-12-28')); // création d'un nouvel employé
-
-/** @var array employees */
-const employees = [employee1]; // tableau contenant les employés
-
-console.log(employee1); // doit afficher l'objet "employee1"
-console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
-console.log(employees); // export des employés dans la console
-
-
-/** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
-
-
-
-// Écrivez votre code à partir de la ligne suivante...
-
+                }
