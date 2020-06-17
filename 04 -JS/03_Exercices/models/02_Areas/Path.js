cabd0f2165a3ed77819dee3812698116aa83d6a9
Brown <brown@MacBook-Pro-de-Brown.local>
2020-06-17 13:39:17 +0200

    Path.js essaie

--- 04 -JS/03_Exercices/models/02_Areas/Path.js
+++ 04 -JS/03_Exercices/models/02_Areas/Path.js
@@ -1,12 +1,38 @@
+
+
+
 class Path {
 
-    constructor (player){
+    /** @var bool 
     
-        this.player(Boolean)
+ 
+    /** @var int/
+   
+ 
+   
+     */
+    constructor(player, nbPassages) {
+
+        this.player = player
+        this.nbPassages = nbPassages;
+
     }
-    
+
+
+
+    getPlayer() {
+        return this.#player;
     }
 
+    getnbPassages() {
+
+        return this.#nbPassages;
+    } 
+
+    setPlayer (player){
 
 
-    
+
+    }
+
+}
