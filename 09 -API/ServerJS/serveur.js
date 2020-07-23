790cd0d137baee01c56048cf98fb4dce839800e5
Brown <brown@mbp-de-brown.home>
2020-07-23 11:17:06 +0200

    api

---
+++ 09 -API/ServerJS/serveur.js
@@ -0,0 +1,26 @@
+// import
+
+import express from 'express';
+
+// instantiate server 
+
+var server = express ();
+
+
+//configure
+// Fonction callback (req = request res = response) 
+
+server.get('/', function (req, res) {
+res.setHeader('content-Type', 'text/html');
+resizeTo.status(200).send(<h1>welcome on my server</h1>);
+
+});
+
+
+//launch server
+
+
+server.listen(8080, function(){
+console.log('flex');
+
+})
