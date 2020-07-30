fa38cf1dc0c0212a8f45786550759523a20ef724
Brown <brown@mbp-de-brown.home>
2020-07-30 10:19:19 +0200

    jason champ

---
+++ 03 -Nodejs/ProgrambyJC/main.js
@@ -0,0 +1,17 @@
+class MusicPlayer {
+    constructor(format) {
+
+      this.format = format;
+
+    }
+
+
+
+
+
+}
+
+let player = new MusicPlayer("mp3");
+let player2 = new MusicPlayer("ogg");
+
+//https://www.youtube.com/watch?v=cnnhogixcI0&list=PLrSOXFDHBtfGxf_PtXLu_OrjFKt4_dqB_&index=8
