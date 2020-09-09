bd504ce8b85212667a6e38d69800368e823e179e
Brown <brown@mbp-de-brown.home>
2020-09-09 14:40:49 +0200

    sql

---
+++ 12 -mysql/ExerciceJasonChampagne/GrouperResultats/GrouperManip.sql
@@ -0,0 +1,25 @@
+use fv_database3;
+
+
+
+
+-- SELECT COUNT(*), user_country 
+-- FROM fv_users
+-- GROUP BY user_country;
+ 
+ 
+ 
+ -- SELECT COUNT(*) AS 'Nombre de résidents ' , user_country 
+-- FROM fv_users
+ -- GROUP BY user_country;
+ 
+ 
+ 
+ -- SELECT user_country , sum(user_age)
+ -- FROM fv_users
+ -- GROUP BY user_country with rollup;
+ 
+ 
+ 
+-- select * from fv_users;
+ -- describe fv_users;
---
+++ 12 -mysql/ExerciceJasonChampagne/modifierSupprimer/ModifierSupprimerJason.sql
@@ -0,0 +1,24 @@
+USE `fv_database2`;
+
+-- C : create (INSERT TO)
+-- R : read (SELECT)
+-- U : update (UPDATE)
+-- D : delete (DELETE)
+
+--  UPDATE fv_games 
+--  SET game_stock = 6, game_price = 11.59
+ -- where id_game = 1; 
+ 
+ 
+ -- ALL
+-- UPDATE fv_games 
+ -- SET game_stock = 6, game_price = 11.59;
+ 
+ 
+ 
+ -- DELETE FROM fv_games where game_title = 'Tekken'; (delete by unit)
+ -- TRUNCATE TABLE fv_games; (delete all)
+ 
+  SELECT * FROM fv_games;
+ 
+ 
