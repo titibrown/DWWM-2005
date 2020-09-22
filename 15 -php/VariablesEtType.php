4baec90be178ec3fda4c8ace8077ff6e685e7799
Brown <brown@MacBook-Pro-de-Brown.local>
2020-09-22 14:34:42 +0200

    Php

---
+++ 15 -php/VariablesEtType.php
@@ -0,0 +1,63 @@
+<?php
+/*
+
+Variables et Type by Jason Champagne
+
+https://www.youtube.com/watch?v=PRXy1yA6O7w
+
+4 Types Scalaires:
+
+Booléen (bollean, bool)         : True or False
+Entier (integer, int)           : 14, -126, 12300
+Flottant (float , double )      :3.14, 12.62
+Chaine de caractère (string)    : "bonjour"
+
+
+4 types composés : 
+
+Tableau (array)
+Objets (Object)
+Fonctions de rappel (callable)
+Itérateurs (iterable)
+
+2 Types divers : 
+Ressource (resource)
+NULL (ou "nul")
+
+Format CamelCase
+
+$maVariable 
+
+
+$monPrenom = "Gina";
+echo "bonjour $monPrenom";
+
+
+
+$unBooleen = true ;
+$unEntier = -14;
+$unFlottant = 3.14;
+$uneChaine = "C'était Hier" 
+
+$unEntier = 0b111111; ( Ecriture Binaire)
+$unEntier = 0xAFD35C; ( Ecriture Hexadecimal)
+$unEntier = 0640 (Octal)
+
+*/
+
+$monPrenom = "Gina";
+echo "bonjour $monPrenom";
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
--- 15 -php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<strong> Bonjour </strong>";
-?>
-
-
-
-
-
-https://www.youtube.com/watch?v=CHcg88F21us&list=PLrSOXFDHBtfFuZttC17M-jNpKnzUL5Adc&index=5
