d2800030a29ca3f7c34d9ffe6e4c79750d7549ed
Brown <brown@MacBook-Pro-de-Brown.local>
2020-09-24 14:37:16 +0200

    php

--- 15 -php/opérateurs.php
+++ 15 -php/opérateurs.php
@@ -3,7 +3,10 @@
 #  https://www.youtube.com/watch?v=FeLf4G800ks&list=PLrSOXFDHBtfFuZttC17M-jNpKnzUL5Adc&index=8
 
 
-# Arithmétiques : + , -, * 
+# Arithmétiques : + , -, * , /, %, **
+# Fonction utiles : intdiv() , fmod()
+
+
 
 $age = 25;
 echo 'Tu as ' .$age. ' ans ';
@@ -19,6 +22,11 @@ $result = $number1 / $number2;
 #Division Entiere 
 
 $result = intdiv($number1, $number2);
+
+$result = fmod(14,26);
+$result = $number1 % $number2;
+
+$result = $number1 ** $number2;
 
 echo $result; 
 
