67efa772eb961d21aff92905fee6b79a1cac9b49
Brown <brown@MacBook-Pro-de-Brown.local>
2020-09-28 11:24:43 +0200

    Suite condition

--- 15 -php/conditions.php
+++ 15 -php/conditions.php
@@ -2,12 +2,16 @@
 
 /* Opérateurs de comparaison : 
 
-    ==, ===, !=,!==
+    ==   : (Egal à)
+    ===  : (identique à : En valeur et en type)
+    !=   : (différent de)
+    !==  : (différence de valeur ou différences de type)
 
     */
 
 
-$number = 14 ; 
+#$number = 14 ; 
+
 
 
 if ($number == 14)
@@ -42,3 +46,36 @@ if ($number !== 14) # Fonctionnement sur le Type  = Egalité et même type
     echo "ok";
 
 }
+
+
+
+
+
+$nb1 = 14;
+$nb2 = 8;
+
+if ($nb1 == 14 xor $nb2 == 8)
+{
+
+echo "OK";
+
+}
+
+if ($nb1 == 14 && $nb2 == 8)
+{
+
+echo "OK";
+
+}
+
+if ($nb1 == 14 or $nb2 == 8)
+{
+
+echo "OK";
+
+}
+
+$var1 = 14;
+$varb=8;
+$result = $var1 <=>  $varB;
+echo $result;
