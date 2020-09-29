9dc9d6d0a0a8e72a00ee978c5e2fb5afa1cbfbea
Brown <brown@MacBook-Pro-de-Brown.local>
2020-09-29 10:55:02 +0200

    End conditions

--- 15 -php/conditions.php
+++ 15 -php/conditions.php
@@ -2,6 +2,9 @@
 
 /* Opérateurs de comparaison : 
 
+
+https://www.youtube.com/watch?v=m2LT_aPPuvk&list=PLrSOXFDHBtfFuZttC17M-jNpKnzUL5Adc&index=9
+
     ==   : (Egal à)
     ===  : (identique à : En valeur et en type)
     !=   : (différent de)
@@ -116,6 +119,56 @@ if ($number > 0)
 }
 
 echo "Suite du programme...";
+
+
+$number=1;
+
+if ($number > 0)
+echo "ok";
+elseif ($number > 5)
+echo "ok";
+else 
+echo"pas ok";
+
+
+
+
+$number=1;
+
+switch ($number)
+{
+case 1:
+    echo "1";
+    break;
+    case 2:
+        echo "2";
+        break;
+        case 3: 
+            echo"3";
+             break;
+            
+            default:
+            break;
+            echo"Pas valide";
+
+}
+
+
+$maVariable = "ok";
+
+
+if (isset($maVariable)) # Est que ma variable est défini? 
+echo "elle existe";
+
+else 
+
+echo"elle n'existe pas";
+
+
+
+
+
+
 
 
 
