7c187ccb1d13b0ab5e7b396ba18485a6e3bc3fb4
Brown <brown@MBP-de-Brown.home>
2020-10-30 08:36:53 +0100

    Capital php

---
+++ 14 -webclient/15 -php/Exo php/16-capitales.php
@@ -0,0 +1,46 @@
+<?php
+
+
+function Capitales($pays){
+
+
+switch($pays){
+
+case"France":
+    return "Paris";
+break;
+
+case"Allemagne":
+    return "Berlin";
+break;
+
+case"Italie":
+    return "Rome";
+break;
+
+case"Espagne":
+    return "Madrid";
+break;
+
+case"Portugal":
+    return "Lisbonne";
+break;
+default;
+
+case"Tout autre pays":
+    return "Inconnu";
+break;
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+?>
