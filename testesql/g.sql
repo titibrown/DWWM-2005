e3508a118c305ab4b7d321cf4ade730a54a8ad42
unknown <timothybrown@free.fr>
2020-11-11 11:26:50 +0100

    s

--- testesql/g.sql
+++ testesql/g.sql
@@ -6,5 +6,7 @@ INSERT INTO `products`
     product_weight)
     VALUES
     
-    ('69.00','Coco','5','Made in Spain','3g')
+    ('69.00','Coco','5','Made in Spain','3g'),
+    ('69.00','Blisse','5','Made in Spain','3g')
+    
     
