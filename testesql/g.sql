d72d4fdcfa13d7699c7a3aa40be8504f164df000
unknown <timothybrown@free.fr>
2020-11-11 11:07:39 +0100

    sql

---
+++ testesql/g.sql
@@ -0,0 +1,10 @@
+use GC; 
+
+INSERT INTO `products` 
+
+    (product_price,product_name,product_stock,product_origin,
+    product_weight)
+    VALUES
+    
+    ('69.00','Coco','5','Made in Spain','3g')
+    
