b8649eb1f94a0d3ce9de909ac7b47c8e35553a9a
unknown <timothybrown@free.fr>
2020-11-16 11:09:26 +0100

    Methode

--- 14 -webclient/15 -php/php Jason/attributsetméthodes.php
+++ 14 -webclient/15 -php/php Jason/attributsetméthodes.php
@@ -1,26 +1,48 @@
 <?php
 
 /*
-Attribut ou proprieté : Variable appartenant à une classe 
+Attribut ou proprieté Private : Variable appartenant à une classe 
 
-Methode : Fonction appartenant a un classe Exemple = __construct()
+Methode Public : Fonction appartenant a un classe Exemple = __construct()
 */
 
 class Vehicle
 
 {
+    // private $_name; 
+    public $_name;
 
-    public $__name;
 
+    public function __construct(string $name, int $power)
 
-    public function __construct()
 {
 
-    echo 'Je suis construit !';
+            $this->_name = $name; 
+
+}
+
+
+public function move(){
+
+
+    // echo'Le véhicule se déplace ';
+     echo $this->_name. ' se déplace ';
+}
 
 
 }
 
-}
+$obj1 = new Vehicle ('Mon bolide');
+$obj1-> move ();
 
-$obj1 = new Vehicle ();
+
+//echo $obj1->_name. '<br>';
+
+/*$obj2 = new Vehicle ('Mon autre bolide');
+echo $obj2->_name. '<br>';*/
+
+
+
+
+
+?>
