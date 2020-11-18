fa061a55e7a5b32521e2d4a88b4aa886c0002a3e
unknown <timothybrown@free.fr>
2020-11-18 16:43:01 +0100

    En mode rage

--- 14 -webclient/15 -php/php_objet_exos/Véhicules/Voiture.php
+++ 14 -webclient/15 -php/php_objet_exos/Véhicules/Voiture.php
@@ -1,34 +1,38 @@
 <?php
 
-
 class Voiture{
 
-    protected $_marque;
-   // protected $_modele;
-   // protected $_poids;
+   protected $_marque;
+   protected $_modele;
+   protected $_poids;
+   protected $_maxspeed ;
 
-public function __construct (string $marque /* string $modele, int $poids*/) {
 
+public function __construct (string $marque, /* string $modele,*/ int $poids, int $maxspeed) {
    $this->_marque = $marque;
-   //$this->_modele = $modele;
-  //  $this->_poids = $poids;
-
+  // $this->_modele = $modele;
+   $this->_poids = $poids;
+   $this->_maxspeed = $maxspeed;
 }
+//public function Caracteristique (){
 
-public function Caracteristique (){
-
-    echo  $this->_marque. 'Skoda';
-
+    // echo  $this->_marque. 'Skoda';
       // $this->_modele. 'Yeti ' ;
-
 //       $this->_poids.  $poids ;
     
-}
+//}
 
+public function Vitessemaximum(){
+
+
+
+return $this->$maxspeed= $marque.$maxspeed - $poids * 30%;
+    
+    }
+    
 }
 
 $voiture = New Voiture ('Mon bolide est une' );
 $voiture-> Caracteristique();
-
 
 ?>
