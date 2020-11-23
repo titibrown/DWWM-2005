4d0a3ba3308d97bf36153b67a8761986774bc878
unknown <timothybrown@free.fr>
2020-11-23 14:49:36 +0100

    Client , intervenant

--- 14 -webclient/15 -php/php_objet_exos/Personnes/Intervenant.php
+++ 14 -webclient/15 -php/php_objet_exos/Personnes/Intervenant.php
@@ -2,46 +2,71 @@
 
 class Intervenant extends Personne  {
 
-    private float $_salaire;
-    private $_poste;
-    private $_revenue;
+    private float  $_salaire;
+    //private string $_poste;
+    private float $_revenue;
 
-    public function __construct( string $poste, float $revenue, float $salaire){
+    public function __construct( string $poste, float $revenue, float $salaire)
+    {
 
-    parent::__construct ();
+        $this->_salaire = $salaire;
+        $this->_revenue = $revenue;
 
-    $this->_salaire = $salaire;
-    $this->_poste = $poste;
-    $this->_revenue = $revenue;
+    }
+
+    public function setSalaire ($salaire)
+    {
+        $this->_salaire = $salaire;
+
+    }
+
+    public function setAutreRevenue($autrerevenue)
+    {
+        $this->_autrerevenue = $revenue 
+
+
+    }
+
+
+    public function getSalaire() : int   
+    {
+
+        return $this->$salaire;
+
+    }
+
+    public function getAutreRevenue() : int
+    {
+
+        return $this->_autrerevenue;
+
+
+    }
+
+
+    public function calculeCharge(float $salaire, float $revenue, string $age, $autrerevenue )
+    {   
+            
+
+            if ($age<'55')
+
+    {
+            $this->($salaire + 20 %) +  ($autrerevenue + 15 %) ; 
+      
+    }   else
+    
+    {
+
+             $this->($salaire + 10 %) + ($autrerevenue + 7.5 %);
+
+    }      
+
+            return $calculeCharge ; 
 
 }
 
-    public function SetSalaire ($salaire){
-
-    $this->_salaire = $salaire;
-
-
-}
-
-    public function GetSalaire(){
-
-    return $this->$salaire;
-
-}
-
-
-    public function CalculeCharge($salaire){
-
-    return $this->$salaire * 20 %  
-
-// + 15 % 
-
-
-}
-
 
 
 
 }
 
-$int1= new Intervenant 
