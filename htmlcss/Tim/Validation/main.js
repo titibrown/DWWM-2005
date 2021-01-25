26f79044cea4093c560eb1a6a618d4b11ccc38ca
Tim <timothybrown@free.fr>
2021-01-25 14:46:26 +0100

    Still

---
+++ htmlcss/Tim/Validation/main.js
@@ -0,0 +1,35 @@
+
+
+/**
+ * inputnumber=document.querySelector('#phonecontainer inpput[type="number]');
+    console.log(inputnumber)
+ * Validation a l'aide de la touche du clavier Enter . 
+ * @keypress
+ * PressEnter.addEventListener('keypress',(event) =>{
+    console.log(event);
+})
+ * ^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$
+ */
+
+
+inputText = document.querySelector('#nameContainer input[type="text]');
+    console.log()
+
+    if ( /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    name.style.backgroundColor ="green";
+    else name.style.backgroundColor ="red";
+
+
+        PressEnter.addEventListener('keypress',(event) =>{
+            console.log(event);
+        })
+
+
+
+
+        inputEmail = document.querySelector('#emailContainer input[type="email"]');
+    console.log()
+
+
+  
+     
