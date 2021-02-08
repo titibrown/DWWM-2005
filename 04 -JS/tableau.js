7264698f817d38db14e9287b0c4e27924f116645
Tim <timothybrown@free.fr>
2021-02-08 11:46:56 +0100

    Tableaux

---
+++ 04 -JS/tableau.js
@@ -0,0 +1,46 @@
+// Crée un tableau 
+
+// let monTableau = new Array('un', 'deux', 'trois');
+// let monTableau = Array('un', 'deux', 'trois');
+let monTableau = ['un', 'deux', 'trois'];
+
+
+// Tableaux Multi-dimensions
+
+
+let monTableau = ['un', 'deux', 'trois'];
+
+// Ancienne version
+let monTableau2D = new Array(
+    Array('Mark', 'Jeff', 'Bill'),
+    Array('Zuckerberg', 'Bezos', 'Gates')  
+);
+
+// Nouvelle version
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+
+
+// Tableaux Associatifs
+
+
+let monTableauAssociatif = {
+    'prenom' : 'Mark',
+    'nom'    : 'Zuckerberg',
+    'poste'  : 'PDG de Facebook'
+};
+
+
+
+// Accéder aux éléments des tableaux
+
+
+let monTableauAssociatif = {
+    'prenom' : 'Mark',
+    'nom'    : 'Zuckerberg',
+    'poste'  : 'PDG de Facebook'
+};
+
+
