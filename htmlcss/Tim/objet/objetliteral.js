4e8e1f3aa955792eb0af94ce7e23b9943def30e2
Tim <timothybrown@free.fr>
2021-02-16 09:23:25 +0100

    js

---
+++ htmlcss/Tim/objet/objetliteral.js
@@ -0,0 +1,57 @@
+let mark = {
+    prenom: "Mark", // propriété
+    nom: "Zuckerberg",
+    email: "mark@facebook.com",
+    
+    // À l'ancienne
+    sePresenter: function(){
+      console.log("Bonjour, je m'appelle " + this.prenom);
+    }
+  }
+  
+  mark.sePresenter();
+  
+  function recevoirLesCoordonnees() {
+    return { latitude: 35, longitude: 139 }
+  }
+  
+  let coordonnees = recevoirLesCoordonnees();
+  console.log(coordonnees.latitude);
+  console.log(coordonnees.longitude);
+
+
+  // On crée une fonction constructeur pour nos utilisateurs
+  
+function Utilisateur(prenom, nom, email) {
+    this.prenom = prenom;
+    this.nom    = nom;
+    this.email  = email;
+    
+    this.sePresenter = () => {
+      console.log("Bonjour, je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email);
+    }
+  }
+  
+  // On crée un objet
+  var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
+  var bill = new Utilisateur("Bill", "Gates", "bill@gatesnotes.com");
+  
+  // mark.sePresenter();
+  // console.log(mark);
+  
+  mark.poste = "PDG de Facebook";
+  
+  // console.log(mark);
+  // console.log(bill);
+  
+  function Logement(type, annee, placeDeParking, proprietaire) {
+    this.type             = type;
+    this.annee            = annee;
+    this.placeDeParking   = placeDeParking;
+    this.proprietaire     = proprietaire;
+  }
+  
+  var appartementA = new Logement('Appartement', 2014, true, mark); // Mark est un objet
+  
+  // console.log(appartementA.proprietaire.prenom);
+  console.log(appartementA);
