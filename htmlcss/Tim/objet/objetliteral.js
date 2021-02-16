ce9109d2f1549f739bb9dc45dde808ce7fea31b5
Tim <timothybrown@free.fr>
2021-02-16 15:00:41 +0100

    regex

--- htmlcss/Tim/objet/objetliteral.js
+++ htmlcss/Tim/objet/objetliteral.js
@@ -21,7 +21,7 @@ let mark = {
 
 
   // On crée une fonction constructeur pour nos utilisateurs
-  
+
 function Utilisateur(prenom, nom, email) {
     this.prenom = prenom;
     this.nom    = nom;
@@ -55,3 +55,67 @@ function Utilisateur(prenom, nom, email) {
   
   // console.log(appartementA.proprietaire.prenom);
   console.log(appartementA);
+
+
+
+  // Constructeur Object
+
+  // Methode n'éffectue aucune opération
+  // Fonction anonyme = This =  not possible 
+
+
+  function Utilisateur(prenom, nom, email) {
+    this.prenom = prenom;
+    this.nom    = nom;
+    this.email  = email;
+  }
+  Utilisateur.prototype.sePresenter = function() {
+      console.log("Bonjour, je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email);
+  }
+  
+  // On crée un objet
+  var mark = new Utilisateur('Mark', 'Zuckerberg', 'mark@facebook.com');
+  var bill = new Utilisateur('Bill', 'Gates', 'bill@gatesnotes.com');
+  
+  console.log(mark); // On voit que JavaScript ajoute une propriété __proto__ et constructor
+  
+  mark.sePresenter();
+  
+  // Créer un objet avec Object()
+  var monObjet = new Object();
+  monObjet.titre = "Le titre de l'objet";
+
+  
+
+
+  // Héritages
+
+
+  // Constructeur de base
+function Animal(nombreDePattes, poids) {
+    this.nombreDePattes = nombreDePattes;
+    this.poids         = poids;
+  }
+  Animal.prototype.presentation = function() {
+    console.log('Cet animal possède ' + this.nombreDePattes + ' pattes et fait ' + this.poids + '.');
+  }
+  
+  // Constructeurs spécialisés
+  function Oiseau(nombreDePattes, poids, longueurDesAiles) {
+    Animal.call(this, nombreDePattes, poids); // La fonction Call() permet d'appeler une fonction constructeur : en premier paramètre on donne l'objet actuel (this), ensuite, on donne les arguments pour son constructeur 
+    this.longueurDesAiles = longueurDesAiles;
+  }
+  Oiseau.prototype = Object.create(Animal.prototype); // On change les informations en donnant un nouvel objet qui sera le prototype de notre constructeur animal
+  Oiseau.prototype.constructor = Oiseau; // On redé finit la valeur de Oiseau.prototype.constructor car elle a été changée avec le changement de prototype
+  
+  function Mammifere(nombreDePattes, poids, typeDePoils) {
+    Animal.call(this, nombreDePattes, poids);
+    this.typeDePoils = typeDePoils;
+  }
+  Mammifere.prototype = Object.create(Animal.prototype);
+  Mammifere.prototype.constructor = Mammifere;
+  
+  var perroquet = new Oiseau(2, '1kg', 'grandes');
+  // console.log(perroquet.nombreDePattes);
+  
+  perroquet.presentation();
---
+++ regex/regex.js
@@ -0,0 +1,35 @@
+const texte =  "me joindre au +33(6)25369854 entre "
+"a fait plus de ventes au 2eme trimestre lorsque"
+"par mail à example@gmail.com ou au +33(1)25364125"
+"+33(2)12365987"
+"avec le devis qui s'élève à un montant de"
+"à en discuter au +33(2)14785421 pour toute question"
+"avec +33% d'augmentation sur l'année dernière"
+"le +33(a)78569852 est invalide"
+
+const lignes = texte.trim().split("\n")
+
+
+console.log("Vérifier si une ligne contient au moins un Match")
+
+
+for(const ligne of lignes){
+    const result =/\+33\(([1-9])\)(\d{2})(\d{2})(\d{2})(\d{2})/.test(ligne)
+    console.log(result ? "Vrai" : "Faux", ligne)
+}
+
+
+console.log("Vérifier qu'une ligne contient un match et sans rien d'autre avant ni après")
+
+for(const ligne of lignes){
+    const result =/^\+33\(([1-9])\)(\d{2})(\d{2})(\d{2})(\d{2})$/.test(ligne)               // Début ^ + Fin $ 
+    console.log(result ? "Vrai" : "Faux", ligne)
+}
+
+
+
+console.log("Extraire tous les Matchs")
+
+
+console.log(texte.match(/\+33\(([1-9])\)(\d{2})(\d{2})(\d{2})(\d{2})/g))   // Flag G permet de ne pas s'aêter au premier Numéro 
+
